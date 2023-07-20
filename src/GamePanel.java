80b4001ce23ee55f908327fd0b7fb6f1c698cf4e
latasdaniel <special@int.pl>
2023-07-20 11:58:28 +0200

    basic movement(not done yet) snake and apple spawn

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -16,7 +16,7 @@ static final int DELAY = 75;
 final int x[]= new int[GAME_UNITS];
 final int y[]= new int[GAME_UNITS];
 
-int bodyParts = 3;
+int bodyParts = 5;
 int applesEaten ;
 int appleX;
 int appleY;
@@ -45,15 +45,53 @@ draw(g);
 
  }
  public void draw(Graphics g){
-for(int i = 0; i<SCREEN_WIDTH/UNIT_SIZE; i++){
+    for(int i = 0; i<SCREEN_WIDTH/UNIT_SIZE; i++){
     g.drawLine(i*UNIT_SIZE,0,i*UNIT_SIZE,SCREEN_HEIGHT);
     g.drawLine(0,i*UNIT_SIZE,SCREEN_WIDTH,i*UNIT_SIZE);
-}
+    }
+
+    g.setColor(Color.green);
+    g.fillOval(appleX,appleY,UNIT_SIZE,UNIT_SIZE);
+
+    for(int i = 0; i < bodyParts ; i++){
+        if(i==0){
+            g.setColor(Color.BLUE);
+            g.fillRect(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
+        }
+        else{
+            g.setColor(Color.WHITE);
+            g.fillRect(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
+        }
+    }
  }
  public void newApple(){
-
+    appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+    appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
  }
  public void move(){
+    for(int i = bodyParts; i > 0 ; i--){
+        x[i] = x[i-1];
+        y[i] = y[i-1];
+    }
+
+    switch (direction){
+        case 'U':
+            y[0] = y[0] - UNIT_SIZE;
+            break;
+
+        case 'D':
+            y[0] = y[0] + UNIT_SIZE;
+            break;
+
+        case 'L':
+            x[0] = x[0] - UNIT_SIZE;
+            break;
+
+        case 'R':
+            x[0] = x[0] + UNIT_SIZE;
+            break;
+
+    }
 
  }
  public void checkApple(){
@@ -74,6 +112,12 @@ for(int i = 0; i<SCREEN_WIDTH/UNIT_SIZE; i++){
   }
     @Override
     public void actionPerformed(ActionEvent e) {
+    if(running){
+        move();
+        checkApple();
+        checkCollisions();
 
+    }
+    repaint();
     }
 }
