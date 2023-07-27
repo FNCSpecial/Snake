8a2318b89c635a9f403ea7dd7c96089837bb6d76
latasdaniel <special@int.pl>
2023-07-27 14:55:52 +0200

    basic movement done, apple eating and respawning then, snake increases size. basic sound when snake eats and dies done as well textures of snake head, body and apple done the game done for now.

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -7,307 +7,295 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Random;
 
-
 public class GamePanel extends JPanel implements ActionListener {
-//game settings and const
-static final int SCREEN_WIDTH = 1080;
-static final int SCREEN_HEIGHT = 720;
-static final int UNIT_SIZE = 20;
-static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / (UNIT_SIZE*UNIT_SIZE);
-static final int DELAY = 50;
+    // game settings and const
+    static final int SCREEN_WIDTH = 1080;
+    static final int SCREEN_HEIGHT = 720;
+    static final int UNIT_SIZE = 20;
+    static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / (UNIT_SIZE * UNIT_SIZE);
+    static final int DELAY = 80;
 
-//snake head and body
-final int[] x = new int[GAME_UNITS];
-final int[] y = new int[GAME_UNITS];
+    // snake head and body
+    final int[] x = new int[GAME_UNITS];
+    final int[] y = new int[GAME_UNITS];
 
-int bodyParts = 5;
+    int bodyParts = 5;
 
-//apples cords and score
-int applesEaten ;
-int appleX;
-int appleY;
-//initial direction and game states
-char direction = 'R';
-boolean running = false;
+    // apples cords and score
+    int applesEaten;
+    int appleX;
+    int appleY;
+    // initial direction and game states
+    char direction = 'R';
+    char[] bodyPartDirection = new char[GAME_UNITS];
+    boolean running = false;
 
-boolean menu = false;
-Timer timer;
-Random random;
+    boolean menu = false;
+    Timer timer;
+    Random random;
 
-private BufferedImage img;
+    private BufferedImage img;
 
-//Object
-Texture tex;
+    // Object
+    Texture tex;
 
+    // panel creation
+    GamePanel() {
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.darkGray);
 
+        BufferedImageLoader loader = new BufferedImageLoader();
+        img = loader.loadImage("/Snake.png");
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+        this.addMouseListener(new MyMouseListener());
+        tex = new Texture();
 
+        startGame();
+    }
 
-//panel creation
-GamePanel() {
-    random = new Random();
-    this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
-    this.setBackground(Color.darkGray);
+    // game starts in menu and timer is set
+    public void startGame() {
+        menu = true;
+        timer = new Timer(DELAY, this);
 
-    BufferedImageLoader loader = new BufferedImageLoader();
-    img = loader.loadImage("/Snake.png");
-    this.setFocusable(true);
-    this.addKeyListener(new MyKeyAdapter());
-    this.addMouseListener(new MyMouseListener());
-    tex = new Texture();
-
-    startGame();
-}
-
-//game starts in menu and timer is set
- public void startGame(){
-    menu=true;
-     timer = new Timer(DELAY,this);
- }
- public void paintComponent(Graphics g){
-super.paintComponent(g);
-draw(g);
-
- }
- //menu draw, game draw and end screen draw
- public void draw(Graphics g) {
-    boolean drown = false;
-        if(menu){
-            gameMenu(g);
+        for (int i = 0; i < bodyParts ; i++) {
+            x[i] = SCREEN_WIDTH / 2; // Set initial X position at the center of the screen
+            y[i] = SCREEN_HEIGHT / 2; // Set initial Y position at the center of the screen
+            bodyPartDirection[i] = direction;
         }
-    else if (running) {
-
-         for (int i = 0; i < SCREEN_WIDTH / UNIT_SIZE; i++) {
-             g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
-             g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
-         }
-
-         g.setColor(Color.green);
-         g.drawImage(tex.apple[0],appleX, appleY,null);
-        // g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
-
-         for (int i = 0; i < bodyParts; i++) {
-             if (i == 0) {
-                 if (direction == 'L') {
-                     g.drawImage(tex.player[0], x[0], y[0], null);
-                 }
-                 if (direction == 'R') {
-                     g.drawImage(tex.player[1], x[0], y[0], null);
-                 }
-                 if (direction == 'U') {
-                     g.drawImage(tex.player[2], x[0], y[0], null);
-                 }
-                 if (direction == 'D') {
-                     g.drawImage(tex.player[3], x[0], y[0], null);
-                 }
-             }
-             else {
-                   g.setColor(Color.GREEN);
-                   g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
-                 }
-             }
-
-
-         g.setColor(Color.red);
-         g.setFont(new Font("Ink Free",Font.BOLD,40));
-         FontMetrics metrics = getFontMetrics(g.getFont());
-         g.drawString("Score:"+applesEaten,(SCREEN_WIDTH - metrics.stringWidth("Score:"+applesEaten)), g.getFont().getSize());
-
-     }
-     else {
-         gameOver(g);
-     }
- }
- //new apple spawn
- public void newApple(){
-    appleX = random.nextInt(SCREEN_WIDTH/UNIT_SIZE)*UNIT_SIZE;
-    appleY = random.nextInt(SCREEN_HEIGHT/UNIT_SIZE)*UNIT_SIZE;
- }
- //moving logic
- public void move(){
-    for(int i = bodyParts; i > 0 ; i--){
-        x[i] = x[i-1];
-        y[i] = y[i-1];
     }
 
-     switch (direction) {
-         case 'U' -> y[0] = y[0] - UNIT_SIZE;
-         case 'D' -> y[0] = y[0] + UNIT_SIZE;
-         case 'L' -> x[0] = x[0] - UNIT_SIZE;
-         case 'R' -> x[0] = x[0] + UNIT_SIZE;
-     }
-
- }
- //checking if head of the snake is in the apple if so add a point and spawn new one
- public void checkApple() throws UnsupportedAudioFileException, LineUnavailableException, IOException {
-if( x[0] == appleX && y[0] == appleY){
-    playSound("src/apple.wav");
-    bodyParts++;
-    applesEaten++;
-    newApple();
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
     }
-}
+
+    // menu draw, game draw and end screen draw
+    public void draw(Graphics g) {
+        if (menu) {
+            gameMenu(g);
+        } else if (running) {
+            for (int i = 0; i < SCREEN_WIDTH / UNIT_SIZE; i++) {
+                g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+                g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
+            }
+
+            g.drawImage(tex.apple[0], appleX, appleY, null);
+
+            switch (direction) {
+                case 'R' -> g.drawImage(tex.player[1], x[0], y[0], null);
+                case 'L' -> g.drawImage(tex.player[0], x[0], y[0], null);
+                case 'U' -> g.drawImage(tex.player[2], x[0], y[0], null);
+                case 'D' -> g.drawImage(tex.player[3], x[0], y[0], null);
+            }
+
+            // Draw the body parts
+            for (int i = 1; i < bodyParts -1; i++) {
+                int textureIndex;
+                int dx = x[i] - x[i - 1];
+
+                if (dx != 0) {
+                    // Horizontal body part
+                    textureIndex = 4;
+                } else {
+                    // Vertical body part
+                    textureIndex = 6;
+                }
+
+                g.drawImage(tex.player[textureIndex], x[i], y[i], null);
+            }
+
+
+
+            g.setColor(Color.red);
+            g.setFont(new Font("Ink Free", Font.BOLD, 40));
+            FontMetrics metrics = getFontMetrics(g.getFont());
+            g.drawString("Score:" + applesEaten, (SCREEN_WIDTH - metrics.stringWidth("Score:" + applesEaten)),
+                    g.getFont().getSize());
+        } else {
+            gameOver(g);
+        }
+    }
+
+    // new apple spawn
+    public void newApple() {
+        appleX = random.nextInt(SCREEN_WIDTH / UNIT_SIZE) * UNIT_SIZE;
+        appleY = random.nextInt(SCREEN_HEIGHT / UNIT_SIZE) * UNIT_SIZE;
+    }
+
+    // moving logic
+    public void move() {
+        // Move the body parts
+        for (int i = bodyParts - 1; i > 0; i--) {
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
+            bodyPartDirection[i] = bodyPartDirection[i - 1];
+        }
+
+        // Move the head based on the current direction
+        switch (direction) {
+            case 'U' -> y[0] -= UNIT_SIZE;
+            case 'D' -> y[0] += UNIT_SIZE;
+            case 'L' -> x[0] -= UNIT_SIZE;
+            case 'R' -> x[0] += UNIT_SIZE;
+        }
+
+        bodyPartDirection[0] = direction;
+    }
+
+
 
     private void playSound(String soundFile) throws IOException, UnsupportedAudioFileException, LineUnavailableException {
         File f = new File(soundFile);
         AudioInputStream audioIn = AudioSystem.getAudioInputStream(f.toURI().toURL());
         Clip clip = AudioSystem.getClip();
+        if (!clip.isRunning())
+            clip.close();
+
         clip.open(audioIn);
         clip.start();
     }
 
-    //collisions logic with snake body and the screen borders
- public void checkCollisions() throws UnsupportedAudioFileException, LineUnavailableException, IOException {
-        //head collisions with body
-        for(int i=bodyParts; i>0; i--){
-            if ((x[0] == x[i]) && y[0] == y[i]) {
+    // checking if head of the snake is in the apple if so add a point and spawn new one
+    public void checkApple() throws UnsupportedAudioFileException, LineUnavailableException, IOException {
+        if (x[0] == appleX && y[0] == appleY) {
+            playSound("src/apple.wav");
+            bodyParts++;
+            applesEaten++;
+            newApple();
+        }
+    }
+
+    // collisions logic with snake body and the screen borders
+    public void checkCollisions() throws UnsupportedAudioFileException, LineUnavailableException, IOException {
+        // head collisions with body
+        for (int i = bodyParts - 1; i > 0; i--) {
+            if (i > 1 && x[0] == x[i] && y[0] == y[i]) {
                 running = false;
                 playSound("src/end.wav");
                 break;
-
             }
         }
-        //head touches borders
-     if(x[0] < 0){
-         playSound("src/end.wav");
-         running = false;
-     }
 
-     if(x[0] > SCREEN_WIDTH){
-         playSound("src/end.wav");
-         running = false;
-     }
+        // head touches borders
+        if (x[0] < 0 || x[0] >= SCREEN_WIDTH || y[0] < 0 || y[0] >= SCREEN_HEIGHT) {
+            playSound("src/end.wav");
+            running = false;
+        }
 
-     if(y[0] < 0){
-         playSound("src/end.wav");
-         running = false;
-     }
-
-     if(y[0] > SCREEN_HEIGHT){
-         playSound("src/end.wav");
-         running = false;
-     }
-
-     if (!running){
-         timer.stop();
-     }
+        if (!running) {
+            timer.stop();
+        }
+    }
 
 
- }
 
- public void gameOver(Graphics g){
-//game over text
-     g.setColor(Color.red);
-     g.setFont(new Font("Ink Free",Font.BOLD,75));
-     FontMetrics metrics1 = getFontMetrics(g.getFont());
-     g.drawString("Game Over",(SCREEN_WIDTH - metrics1.stringWidth("Game Over"))/2, SCREEN_HEIGHT/2);
 
-     g.setColor(Color.red);
-     g.setFont(new Font("Ink Free",Font.BOLD,40));
-     FontMetrics metrics2 = getFontMetrics(g.getFont());
-     g.drawString("Score:"+applesEaten,(SCREEN_WIDTH - metrics2.stringWidth("Score:"+applesEaten))/2, (SCREEN_HEIGHT/2)+50);
- }
-//game menu text
+
+    public void gameOver(Graphics g) {
+        // game over text
+        g.setColor(Color.red);
+        g.setFont(new Font("Ink Free", Font.BOLD, 75));
+        FontMetrics metrics1 = getFontMetrics(g.getFont());
+        g.drawString("Game Over", (SCREEN_WIDTH - metrics1.stringWidth("Game Over")) / 2, SCREEN_HEIGHT / 2);
+
+        g.setColor(Color.red);
+        g.setFont(new Font("Ink Free", Font.BOLD, 40));
+        FontMetrics metrics2 = getFontMetrics(g.getFont());
+        g.drawString("Score:" + applesEaten, (SCREEN_WIDTH - metrics2.stringWidth("Score:" + applesEaten)) / 2,
+                (SCREEN_HEIGHT / 2) + 50);
+    }
+
+    // game menu text
     public void gameMenu(Graphics g) {
         g.setColor(Color.WHITE);
-        g.setFont(new Font("Ink Free",Font.BOLD,75));
+        g.setFont(new Font("Ink Free", Font.BOLD, 75));
         FontMetrics metrics1 = getFontMetrics(g.getFont());
-        g.drawString("Menu",(SCREEN_WIDTH - metrics1.stringWidth("Menu"))/2, g.getFont().getSize());
+        g.drawString("Menu", (SCREEN_WIDTH - metrics1.stringWidth("Menu")) / 2, g.getFont().getSize());
 
-        g.drawString("Start",(SCREEN_WIDTH - metrics1.stringWidth("Start"))/2,SCREEN_HEIGHT/2 + 60);
-        g.draw3DRect(SCREEN_WIDTH/2 - 150,SCREEN_HEIGHT/2,300,80,true);
-
-
-
-
+        g.drawString("Start", (SCREEN_WIDTH - metrics1.stringWidth("Start")) / 2, SCREEN_HEIGHT / 2 + 60);
+        g.draw3DRect(SCREEN_WIDTH / 2 - 150, SCREEN_HEIGHT / 2, 300, 80, true);
     }
-//keys action to change the movement
-  public class MyKeyAdapter extends KeyAdapter{
 
-      @Override
-      public void keyPressed(KeyEvent e) {
-          switch (e.getKeyCode()) {
-              case KeyEvent.VK_LEFT, KeyEvent.VK_A -> {
-                  if (direction != 'R') {
-                      direction = 'L';
-                  }
-              }
-              case KeyEvent.VK_RIGHT,KeyEvent.VK_D -> {
-                  if (direction != 'L') {
-                      direction = 'R';
-                  }
-              }
-              case KeyEvent.VK_UP,KeyEvent.VK_W  -> {
-                  if (direction != 'D') {
-                      direction = 'U';
-                  }
-              }
-              case KeyEvent.VK_DOWN,KeyEvent.VK_S -> {
-                  if (direction != 'U') {
-                      direction = 'D';
-                  }
-              }
+    // keys action to change the movement
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_LEFT, KeyEvent.VK_A -> {
+                    if (direction != 'R') {
+                        direction = 'L';
+                    }
+                }
+                case KeyEvent.VK_RIGHT, KeyEvent.VK_D -> {
+                    if (direction != 'L') {
+                        direction = 'R';
+                    }
+                }
+                case KeyEvent.VK_UP, KeyEvent.VK_W -> {
+                    if (direction != 'D') {
+                        direction = 'U';
+                    }
+                }
+                case KeyEvent.VK_DOWN, KeyEvent.VK_S -> {
+                    if (direction != 'U') {
+                        direction = 'D';
+                    }
+                }
+            }
+        }
+    }
 
-          }
-      }
-
-  }
-
-    public class MyMouseListener implements MouseListener{
-//mouse action to click start game and restart the game
+    public class MyMouseListener implements MouseListener {
+        // mouse action to click start game and restart the game
 
         @Override
         public void mouseClicked(MouseEvent e) {
             int pointX = e.getX();
             int pointY = e.getY();
 
-          if(pointX > 360 && pointX < 690 && pointY > 360 && pointY < 440){
-              menu=false;
-              newApple();
-              running=true;
-
-              timer.start();
-          }
-
-
+            if (pointX > 360 && pointX < 690 && pointY > 360 && pointY < 440) {
+                menu = false;
+                newApple();
+                running = true;
+                timer.start();
+            }
         }
 
         @Override
         public void mousePressed(MouseEvent e) {
-
         }
 
         @Override
         public void mouseReleased(MouseEvent e) {
-
         }
 
         @Override
         public void mouseEntered(MouseEvent e) {
-
         }
 
         @Override
         public void mouseExited(MouseEvent e) {
-
         }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-    if(running){
-        move();
-        try {
-            checkApple();
-        } catch (UnsupportedAudioFileException | LineUnavailableException | IOException ex) {
-            throw new RuntimeException(ex);
+        if (running) {
+            move();
+            try {
+                checkApple();
+            } catch (UnsupportedAudioFileException | LineUnavailableException | IOException ex) {
+                throw new RuntimeException(ex);
+            }
+            try {
+                checkCollisions();
+            } catch (UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
+                throw new RuntimeException(ex);
+            }
         }
-        try {
-            checkCollisions();
-        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
-            throw new RuntimeException(ex);
-        }
-
-    }
-    repaint();
+        repaint();
     }
 }
--- src/Texture.java
+++ src/Texture.java
@@ -5,7 +5,7 @@ public class Texture {
     private BufferedImage apple_sheet = null;
     private BufferedImage player_sheet = null;
 
-    public BufferedImage[] player = new BufferedImage[15];
+    public BufferedImage[] player = new BufferedImage[19];
     public BufferedImage[] apple = new BufferedImage[1];
     public Texture(){
 
@@ -28,8 +28,25 @@ public class Texture {
         player[1] = ps.grabImage(5,1,20,20); //head left
         player[2] = ps.grabImage(4,1,20,20); //head up
         player[3] = ps.grabImage(5,2,20,20); //head down
+
         player[4] = ps.grabImage(2,1,20,20); //body horizontally
-        player[5] = ps.grabImage(3,2,20,20); //body vertically
+        player[6] = ps.grabImage(3,2,20,20); //body vertically
+
+        player[7] = ps.grabImage(3,1,20,20); //body horizontally turn down - right
+        player[8] = ps.grabImage(3,3,20,20); //body horizontally turn up   - right
+        player[9] = ps.grabImage(1,1,20,20); //body horizontally turn down - left
+        player[10] =ps.grabImage(1,2,20,20); //body horizontally turn up   - left
+
+        player[11] = ps.grabImage(1,1,20,20); //body vertically  turn up   - right
+        player[12] = ps.grabImage(3,1,20,20); //body vertically  turn up   - left
+        player[13] = ps.grabImage(1,2,20,20); //body vertically  turn down - right
+        player[14] = ps.grabImage(3,3,20,20); //body vertically  turn down - left
+
+        player[15] = ps.grabImage(3,2,20,20); //tail up
+        player[16] = ps.grabImage(3,2,20,20); //tail right
+        player[17] = ps.grabImage(3,2,20,20); //tail left
+        player[18] = ps.grabImage(3,2,20,20); //tail down
+
         apple[0] = as.grabImage(1,4,20,20); //apple
 
     }
